ec93482d228bc5d95fc5a0e52b3efb0566ff08d7
sharebravery <sharebravery@gmail.com>
2023-08-10 20:02:12 +0800

    feat: strlen

--- strfun.c
+++ strfun.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
 
+size_t strlen(const char *str)
+{
+    int length;
+    while (*str != '\0')
+    {
+        length++;
+        str++; // 指针后移
+    }
+
+    return length;
+}
+
 int main()
 {
     const char str[] = "HelloWorld";
+
+    size_t len = strlen(str);
+
+    printf("%zu\n", len);
 
     return 0;
 }
